022d23d8ab83cdbf7f9624b8e0a800aabe67059e
jesu-m0 <jesusmd2000@gmail.com>
2024-08-17 17:43:15 +0200

    some small fixed ive should commit before and changing of landing page and change of the navbar

---
+++ frontend/portfolioJMD/src/app/models/proyecto.model.ts
@@ -0,0 +1,11 @@
+export class Proyecto {
+    constructor(
+      public title: string,
+      public date: string,
+      public shortDescription: string,
+      public longDescription: string,
+      public photos: string[], 
+      public videos: string[] 
+    ) {}
+  }
+  
