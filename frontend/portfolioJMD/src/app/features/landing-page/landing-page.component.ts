10d65e4b6227ad4a38b25a4e33f52e5486252795
jesu-m0 <jesusmd2000@gmail.com>
2024-08-18 19:54:30 +0200

    tech skills  added

--- frontend/portfolioJMD/src/app/features/landing-page/landing-page.component.ts
+++ frontend/portfolioJMD/src/app/features/landing-page/landing-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../sharedComponents/navbar/navbar.component';
 import { LinkButtonComponentComponent } from '../../sharedComponents/link-button-component/link-button-component.component';
+import { SkillSectionComponent } from './components/skill-section/skill-section.component';
 
 @Component({
   selector: 'landing-page',
   standalone: true,
   imports: [NavbarComponent,
-    LinkButtonComponentComponent
+    LinkButtonComponentComponent,
+    SkillSectionComponent
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
