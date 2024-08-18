3b3b1a7bda6e80288ca1052878fe7d964a5ca6cd
jesu-m0 <jesusmd2000@gmail.com>
2024-08-18 15:41:59 +0200

    linkbutton component created

--- frontend/portfolioJMD/src/app/features/landing-page/landing-page.component.ts
+++ frontend/portfolioJMD/src/app/features/landing-page/landing-page.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../sharedComponents/navbar/navbar.component';
+import { LinkButtonComponentComponent } from '../../sharedComponents/link-button-component/link-button-component.component';
 
 @Component({
   selector: 'landing-page',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,
+    LinkButtonComponentComponent
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent {
-  
+
 
 }
--- frontend/portfolioJMD/src/app/features/projects/components/projects-timeline-card/projects-timeline-card.component.ts
+++ frontend/portfolioJMD/src/app/features/projects/components/projects-timeline-card/projects-timeline-card.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { LinkButtonComponentComponent } from '../../../../sharedComponents/link-button-component/link-button-component.component';
 
 @Component({
   selector: 'app-projects-timeline-card',
   standalone: true,
-  imports: [],
+  imports: [
+    LinkButtonComponentComponent
+  ],
   templateUrl: './projects-timeline-card.component.html',
   styleUrl: './projects-timeline-card.component.css'
 })
