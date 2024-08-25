8f9d2e0277bf7df73a93ca5443a94bfd6f3d32f6
jesu-m0 <jesusmd2000@gmail.com>
2024-08-25 21:04:15 +0200

    contact formular created but not finished

--- frontend/portfolioJMD/src/app/features/about-me/about-me.component.ts
+++ frontend/portfolioJMD/src/app/features/about-me/about-me.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SkillsSectionComponent } from './components/skills-section/skills-section.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [SkillsSectionComponent],
+  imports: [SkillsSectionComponent, ContactFormComponent],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
