b813646cd9750ebab667fe3844cf2d413303b605
jesu-m0 <jesusmd2000@gmail.com>
2024-08-27 16:19:08 +0200

    project button

--- frontend/portfolioJMD/src/app/features/about-me/about-me.component.ts
+++ frontend/portfolioJMD/src/app/features/about-me/about-me.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SkillsSectionComponent } from './components/skills-section/skills-section.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [SkillsSectionComponent, ContactFormComponent],
+  imports: [SkillsSectionComponent, ContactFormComponent, RouterModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
