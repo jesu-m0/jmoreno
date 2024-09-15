8e8b6cb604b11ac18d65810d3237286ca15a8d59
jesu-m0 <jesusmd2000@gmail.com>
2024-09-15 13:24:08 +0200

    Some details added and contact form working

--- frontend/portfolioJMD/src/app/features/about-me/components/contact-form/contact-form.component.ts
+++ frontend/portfolioJMD/src/app/features/about-me/components/contact-form/contact-form.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient  } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule, 
+  ],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.css'
+  styleUrl: './contact-form.component.css' 
 })
-export class ContactFormComponent {
 
+
+export class ContactFormComponent {
+  constructor(private http: HttpClient) {}
+
+  public sendEmail(event: Event) {
+    event.preventDefault(); // Prevent default form behavior
+
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
+
+    // Convert FormData to a JSON object
+    const data: any = {};
+    formData.forEach((value, key) => (data[key] = value));
+
+    // Make a POST request to the serverless function
+    this.http.post('/api/sendMail', data)
+      .subscribe({
+        next: (response) => {
+          console.log('Emails sent successfully', response);
+        },
+        error: (error) => {
+          console.error('Error sending emails', error);
+        }
+      });
+  }
 }
