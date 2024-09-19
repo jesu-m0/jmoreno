0c1ac2d72d2e48ce1a00d401ea7099ec1a36a597
jesu-m0 <jesusmd2000@gmail.com>
2024-09-19 09:56:14 +0200

    font changed

--- frontend/portfolioJMD/src/app/app.component.ts
+++ frontend/portfolioJMD/src/app/app.component.ts
@@ -16,19 +16,4 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent{
   title = 'portfolioJMD';
-  fontClasses: string[] = ['montserrat-400', 'poppins-regular', 'rem-600', 'default'];
-  currentFontIndex: number = 0;
-  currentFontClass: string = this.fontClasses[this.currentFontIndex];
-
-  toggleFont() {
-    // Remover la clase anterior del body
-    document.body.classList.remove(this.currentFontClass);
-
-    // Cambiar al siguiente índice de fuente
-    this.currentFontIndex = (this.currentFontIndex + 1) % this.fontClasses.length;
-
-    // Obtener la nueva clase y aplicarla al body
-    this.currentFontClass = this.fontClasses[this.currentFontIndex];
-    document.body.classList.add(this.currentFontClass);
-  }
 }
