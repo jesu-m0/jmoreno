322a8091b5cb12e48c20cd7f6ee504b4be3a9399
jesu-m0 <jesusmd2000@gmail.com>
2025-05-23 23:31:37 +0200

    career hint finished

--- src/app/sections/landing-page/components/career-hint/career-hint.component.ts
+++ src/app/sections/landing-page/components/career-hint/career-hint.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RedirectButtonComponent } from "../../../../shared/redirect-button/redirect-button.component";
 
 @Component({
   selector: 'app-career-hint',
-  imports: [],
+  imports: [
+    RedirectButtonComponent
+  ],
   templateUrl: './career-hint.component.html',
   styleUrl: './career-hint.component.css'
 })
