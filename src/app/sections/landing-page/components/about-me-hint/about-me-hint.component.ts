6a8ec05bda8a197ab6599541ff5eae0f0f1de645
jesu-m0 <jesusmd2000@gmail.com>
2025-05-24 14:15:18 +0200

    main skills on landing page done

--- src/app/sections/landing-page/components/about-me-hint/about-me-hint.component.ts
+++ src/app/sections/landing-page/components/about-me-hint/about-me-hint.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RedirectButtonComponent } from '../../../../shared/redirect-button/redirect-button.component';
+import { MainSkillsHintComponent } from '../main-skills-hint/main-skills-hint.component';
 
 @Component({
   selector: 'app-about-me-hint',
   imports: [
-    RedirectButtonComponent
+    RedirectButtonComponent,
+    MainSkillsHintComponent,
   ],
   templateUrl: './about-me-hint.component.html',
   styleUrl: './about-me-hint.component.css'
