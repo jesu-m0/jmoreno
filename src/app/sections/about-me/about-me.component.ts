f4a9d2b8de673426a33cbd43fced475364ca8c14
jesu-m0 <jesusmd2000@gmail.com>
2025-05-25 21:09:30 +0200

    about me first text and skill section started

--- src/app/sections/about-me/about-me.component.ts
+++ src/app/sections/about-me/about-me.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from "../../shared/elements/title/title.component";
+import { RouterLink } from '@angular/router';
+import { SubsectionComponent } from "../../shared/elements/subsection/subsection.component";
 
 @Component({
   selector: 'app-about-me',
-  imports: [],
+  imports: [
+    TitleComponent,
+    RouterLink,
+    SubsectionComponent
+],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
