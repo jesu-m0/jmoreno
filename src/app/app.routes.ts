0fe6eb1d7a4d5908f8a4190f6d86336c3c68c676
jesu-m0 <jesusmd2000@gmail.com>
2025-05-25 20:09:36 +0200

    navigation created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './sections/landing-page/landing-page.component';
+import { AboutMeComponent } from './sections/about-me/about-me.component';
+import { CareerComponent } from './sections/career/career.component';
+import { ProjectsComponent } from './sections/projects/projects.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
+    { path: 'about', component: AboutMeComponent },
+    { path: 'career', component: CareerComponent },
+    { path: 'projects', component: ProjectsComponent },
 
     { path: '**', redirectTo: '' }
 ];
