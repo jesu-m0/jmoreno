4dfc77f99792dc1593cc58b765f20aff55d9f0f1
jesu-m0 <jesusmd2000@gmail.com>
2025-05-26 13:13:28 +0200

    about me skills done

--- src/app/sections/about-me/about-me.component.ts
+++ src/app/sections/about-me/about-me.component.ts
@@ -3,6 +3,7 @@ import { TitleComponent } from "../../shared/elements/title/title.component";
 import { RouterLink } from '@angular/router';
 import { SubsectionComponent } from "../../shared/elements/subsection/subsection.component";
 import { ContactMeComponent } from "./components/contact-me/contact-me.component";
+import { SkillsComponent } from "./components/skills/skills.component";
 
 @Component({
   selector: 'app-about-me',
@@ -10,7 +11,8 @@ import { ContactMeComponent } from "./components/contact-me/contact-me.component
     TitleComponent,
     RouterLink,
     SubsectionComponent,
-    ContactMeComponent
+    ContactMeComponent,
+    SkillsComponent
 ],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
--- src/app/sections/landing-page/components/project-hint/project-hint.component.ts
+++ src/app/sections/landing-page/components/project-hint/project-hint.component.ts
@@ -3,6 +3,7 @@ import { ProjectCardComponent } from '../../../../shared/elements/project-card/p
 import { ProjectCardV2Component } from '../../../../shared/elements/project-card-v2/project-card-v2.component';
 import { RedirectButtonComponent } from "../../../../shared/elements/redirect-button/redirect-button.component";
 import { ProjectCardV3Component } from "../../../../shared/elements/project-card-v3/project-card-v3.component";
+import { TitleComponent } from "../../../../shared/elements/title/title.component";
 
 @Component({
   selector: 'app-project-hint',
@@ -10,7 +11,8 @@ import { ProjectCardV3Component } from "../../../../shared/elements/project-card
     ProjectCardComponent,
     ProjectCardV2Component,
     RedirectButtonComponent,
-    ProjectCardV3Component
+    ProjectCardV3Component,
+    TitleComponent
 ],
   templateUrl: './project-hint.component.html',
   styleUrl: './project-hint.component.css'
