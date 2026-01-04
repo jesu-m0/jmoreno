9e16e6a5116ba380bbf5fa4915e88c1bbedc4275
jesu-m0 <jesusmd2000@gmail.com>
2026-01-04 20:38:39 +0100

    enable PWA
    Added @angular/pwa for faster loads and offline support. Service worker caches app shell and assets on-demand.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 // app/app.config.ts
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import {
   provideRouter,
   withInMemoryScrolling,
@@ -8,6 +8,7 @@ import {
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -25,6 +26,9 @@ export const appConfig: ApplicationConfig = {
       withRouterConfig({
         onSameUrlNavigation: 'reload'
       }),
-    ),
+    ), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 };
