ab3bfc7377ba1e68b7cec3deb4d47e9326e3e63b
jesu-m0 <jesusmd2000@gmail.com>
2026-01-04 20:27:14 +0100

    ng update and career update

--- src/app/core/services/experience.service.ts
+++ src/app/core/services/experience.service.ts
@@ -8,10 +8,22 @@ import { Experience } from '../models/experience.model';
 export class ExperienceService {
   private professionalExperience: Experience[] = [
     {
+      company: 'Otto Group one.O',
+      role: 'Software Engineer',
+      location: 'Málaga, Andalusia, Spain',
+      period: 'Sept 2025 - Present',
+      roleHighlights: [
+        "Building internal business tools and dashboards with <span class='text-black'>Svelte</span> for the frontend and <span class='text-black'>Spring Boot</span> on the backend, using both <span class='text-black'>PostgreSQL</span> and <span class='text-black'>MongoDB</span> depending on the use case.",
+        "Updatin and developing new features from scratch based on business requirements, making decisions on architecture and implementation details along the way.",
+        "Setting up and managing <span class='text-black'>Docker and Kubernetes</span> deployments to keep things running smoothly across environments.",
+        "Working on <span class='text-black'>CI/CD pipelines</span> using <span class='text-black'>GitLab</span> to automate builds and deployments.",
+      ],
+    },
+    {
       company: 'EDAG Group',
       role: 'Software Engineer',
       location: 'Fulda, Hesse, Germany',
-      period: 'Sept 2022 - Nov 2024',
+      period: 'Dec 2023 - Feb 2025',
       roleHighlights: [
         "Developed a tool to manage and assign staff to support shifts, starting from gathering requirements and creating functional diagrams. The frontend was built using <span class='text-black'>Angular 18</span> with <span class='text-black'>Tailwind CSS</span> and DaisyUI, while the backend was powered by <span class='text-black'>Spring Boot with JPA and PostgreSQL</span>. Automated manual processes and improved resource planning efficiency.",
         "Handled Dockerization using <span class='text-black'>Docker Compose</span>. Ensured seamless deployment, replication, and management of services across multiple environments.",
